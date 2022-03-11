a0f78fccf47cd2e6a2a7b541e044993241f51b8b
bibouche_ <mil.somnn@gmail.com>
2022-03-11 20:03:55 +0100

    CommandInvitation implemented

--- src/main/java/fr/alcanderia/plugin/survivalteams/commands/CommandAll.java
+++ src/main/java/fr/alcanderia/plugin/survivalteams/commands/CommandAll.java
@@ -18,6 +18,8 @@ public class CommandAll implements CommandExecutor, TabCompleter {
         switch (args[0]) {
             case "confirmation":
                 return new CommandConfirmation().onCommand(sender, command, label, newArgs);
+            case "invitation":
+                return new CommandInvitation().onCommand(sender, command, label, newArgs);
             case "list":
                 return new CommandList().onCommand(sender, command, label, new String[0]);
             case "top":
@@ -61,6 +63,8 @@ public class CommandAll implements CommandExecutor, TabCompleter {
                     break;
                 case "confirmation":
                     completions.addAll(new CommandConfirmation().onTabComplete(sender, command, alias, newArgs));
+                case "invitation":
+                    completions.addAll(new CommandInvitation().onTabComplete(sender, command, alias, newArgs));
                     break;
                 case "top":
                     completions.addAll(new CommandTop().onTabComplete(sender, command, alias, newArgs));
@@ -94,6 +98,7 @@ public class CommandAll implements CommandExecutor, TabCompleter {
 
     public static void regCommands() {
         regCommand("confirmation", "/st confirmation confirm|cancel", "Allows you to confirm a commands that has high power");
+        regCommand("invitation", "/st invitation accept|decline", "Allows you to confirm an invitation you have received");
         regCommand("top", "/st top <maxRank>", "Displays a top of the teams, from 1st to the parameter you specify");
         regCommand("info", "/st info team|player <team>|<player>", "Returns the informations concerning a specific team/player");
         regCommand("create", "/st create <teamName>", "Allows you to create a team");
